d409aeaa2073d588a5b673d0ce5f3b814fc9900f
deadlightreal <richardfabianmain@gmail.com>
2025-09-10 15:47:42 +0200

    changed app location to not be system location

--- src/utils/file_system/application_directory.cpp
+++ src/utils/file_system/application_directory.cpp
@@ -1,12 +1,23 @@
 #include "file_system.hpp"
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
 #include <string>
 
 std::string utils::file_system::get_application_directory() {
 
 #ifdef __APPLE__
-    std::string result = std::string("/Library/Application Support/swift_com");
+    const char* home_env = std::getenv("HOME");
+    if (home_env == nullptr) {
+        perror("Failed to get home env");
+        exit(EXIT_FAILURE);
+    }
+    
+    std::string result = std::string(home_env);
+
+    result += "/Library/Application Support/swift_com";
+
+    std::cout << "Location: " << result << std::endl;
 
     return result;
 #endif
